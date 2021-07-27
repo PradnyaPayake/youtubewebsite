bcdbf04ea8b8c017f5a154f1538a9640b73dc987
PradnyaPayake <88047805+PradnyaPayake@users.noreply.github.com>
2021-07-27 21:25:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;400&family=Ubuntu&display=swap" rel="stylesheet">
+
+  </head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><b>Health Care polyclinic</b></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Home<span class="sr-only">
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="#">Services</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+        </li>
+
+
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/img7.jpg" alt="Los Angeles" width="500" height="500">
+      <div class="carousel-caption">
+        <h3>Welcome to Health Care Polyclinic:)</h3>
+        
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/hp2.jpg" alt="Chicago" width="500" height="500">
+      <div class="carousel-caption">
+        
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/hp4.jpg" alt="New York" width="500" height="500">
+      <div class="carousel-caption">
+        
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<section class="my-6">
+	<div class="py-5">
+		<h2 class="text-center"><b>About us</b></h2>
+	</div>
+	<div class="container-fluid">
+		
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-12">
+			<img src="images/img6.jpg" class="img-fluid aboutimg">
+			
+		</div>
+		<div class="col-lg-6 col-md-6 col-12">
+			<h2 class="display-4">Health Care Polyclinic</h2>
+			<p class="py-4">Care beyond limits(24x7)</p>
+      <p><b>Contact Us:</b></p>
+      <p>8149402155/8149299348</p>
+			<a href="about.php" class="btn btn-success"> check more</a>
+			
+		</div>
+		
+	</div>
+</div>
+</section>
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center"><b>Doctors Team</b></h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      
+      <div class="col-lg-4 col-md-4 col-12">
+<div class="card">
+  <img class="card-img-top" src="images/sw2.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Dr.Swapnil L. Pawar</h4>
+    <p class="card-text">Family Physician</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+
+
+
+        
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+<div class="card">
+  <img class="card-img-top" src="images/sw4.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Dr.Swapnil V. More</h4>
+    <p class="card-text">Opthalmologist</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+
+
+
+        
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+<div class="card">
+  <img class="card-img-top" src="images/sa3.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Dr.Sachin B.Ingle</h4>
+    <p class="card-text">Consulting Pediatrician</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+
+</div>
+      <div class="col-lg-4 col-md-4 col-12">
+<div class="card">
+  <img class="card-img-top" src="images/sa5.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Dr.Sachin G.Pagde</h4>
+    <p class="card-text">General Surgeon</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+
+
+</div>
+      <div class="col-lg-4 col-md-4 col-12">
+<div class="card">
+  <img class="card-img-top" src="images/pr3.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Dr.Pradnya B.Palekar</h4>
+    <p class="card-text">OBS & GYNAE Specialist</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+
+
+
+
+        
+      </div>
+    </div>
+    
+  </div>
+</section>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center"><b>Our services</b></h2>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/se1.jpg" class="img-fluid pb-4">
+        
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/se2.jpg" class="img-fluid pb-4">
+        
+      </div>
+      
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/se3.jpg" class="img-fluid pb-4">
+        
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/se4.jpg" class="img-fluid pb-4">
+        
+      </div>
+      
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/se5.jpg" class="img-fluid pb-4">
+        
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/se6.jpg" class="img-fluid pb-4">
+        
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/se7.jpg" class="img-fluid pb-4">
+        
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/se8.jpg" class="img-fluid pb-4">
+        
+      </div>
+      
+      
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/se9.jpg" class="img-fluid pb-4">
+        
+      </div>
+      
+      
+    </div>
+    
+
+  </div>
+  </section>
+
+  <section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center"><b>Details</b></h2>
+  </div>
+  <div class="w-50 m-auto" >
+    <form action="userinfo.php" method="post">
+      <div class="form-group">
+        <label>Username</label>
+        <input type="text" name="user" autocomplete="off" class="form-control">
+        
+      </div>
+      <div class="form-group">
+        <label>email id</label>
+        <input type="text" name="email" autocomplete="off" class="form-control">
+        
+      </div>
+      <div class="form-group">
+        <label>Mobile no</label>
+        <input type="text" name="Mobile" autocomplete="off" class="form-control">
+        
+      </div>
+      <div class="dropdown">
+        
+  <button type="text" class=" form control dropdown-toggle" data-toggle="dropdown">
+    Select doctor
+  </button>
+  <div class="dropdown-menu">
+   
+    <a class="dropdown-item" href="#">Dr.Swapnil Pawar</a>
+    <a class="dropdown-item" href="#">Dr.Swapnil More</a>
+    <a class="dropdown-item" href="#">Dr.Sachin Ingle</a>
+    <a class="dropdown-item" href="#">Dr.Sachin Pagde</a>
+    <a class="dropdown-item" href="#">Dr.Pradnya Palekar</a>
+  </div>
+</div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+      
+    </form>
+    
+  </div>
+</section>
+
+
+
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@healthcarepolyclinic</p>
+  <p>Shivprathisthan Chowk,Khandoba Mandir,Last Bus Stop,Near Gangotri Hotel,Sukhsagar Nagar,Katraj Pune-411046</p>
+</footer>
+	
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+
+</body>
+</html>
