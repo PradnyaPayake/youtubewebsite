557262121cc2f1d26570f7a9d4046d97a336cac4
PradnyaPayake <88047805+PradnyaPayake@users.noreply.github.com>
2021-07-27 21:25:59 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200&family=Ubuntu&display=swap" rel="stylesheet">
+  
+
+  </head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Health care polyclinic</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.php">Home<span class="sr-only">
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Services</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link disabled" href="about.php">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+        </li>
+
+
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1>Health care Polyclinic</h1>
+  <p>Care beyond limits(24x7)</p>
+  <p>Contact Us:</p>
+  <p>8149402155/8149299348</p>
+</div>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center"><b>About us</b></h2>
+  </div>
+  <div class="container-fluid">
+    
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="images/img4.jpg" class="img-fluid aboutimg">
+      
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+      <h2 class="display-4">Health Care Polyclinic</h2>
+      <p><b>Address:</b></p>
+      <p class="py-3">Shivprathisthan Chowk,Khandoba Mandir,Last Bus Stop,Near Gangotri Hotel,Sukhsagar Nagar,Katraj Pune-411046</p>
+      <a href="about.php" class="btn btn-success"> check more</a>
+      
+    </div>
+    
+  </div>
+</div>
+</section>
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@healthcareclinic</p>
+</footer>
+</body>
+</html>
+
+
+
+
